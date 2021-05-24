9062376101a3cef3f0977c49f9e63071f199092c
n4to4 <n4to4k@gmail.com>
2021-05-24 20:19:04 +0900

    snapshot

--- 03/channels.go
+++ 03/channels.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"sync"
+)
 
 func main0() {
 	stringStream := make(chan string)
@@ -18,7 +23,7 @@ func main1() {
 	fmt.Printf("(%v): %v\n", ok, integer)
 }
 
-func main() {
+func main2() {
 	intStream := make(chan int)
 	go func() {
 		defer close(intStream)
@@ -31,4 +36,40 @@ func main() {
 		fmt.Printf("%v ", integer)
 	}
 	fmt.Println("")
+}
+
+func main3() {
+	begin := make(chan interface{})
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			<-begin
+			fmt.Printf("%v has begun\n", i)
+		}(i)
+	}
+
+	fmt.Println("Unblocking goroutines...")
+	close(begin)
+	wg.Wait()
+}
+
+func main() {
+	var stdoutBuff bytes.Buffer
+	defer stdoutBuff.WriteTo(os.Stdout)
+
+	intStream := make(chan int, 4)
+	go func() {
+		defer close(intStream)
+		defer fmt.Println(&stdoutBuff, "Producer Done.")
+		for i := 0; i < 5; i++ {
+			fmt.Fprintf(&stdoutBuff, "Sending: %d\n", i)
+			intStream <- i
+		}
+	}()
+
+	for integer := range intStream {
+		fmt.Fprintf(&stdoutBuff, "Received %v.\n", integer)
+	}
 }
