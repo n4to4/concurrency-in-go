6bdfeef1a37ab12db7660d9b268a6d6e15a98ccf
n4to4 <n4to4k@gmail.com>
2021-05-28 21:02:51 +0900

    snapshot

--- 03/select.go
+++ 03/select.go
@@ -5,17 +5,30 @@ import (
 	"time"
 )
 
-func main() {
-	start := time.Now()
-	c := make(chan interface{})
-	go func() {
-		time.Sleep(5 * time.Second)
-		close(c)
-	}()
+func main0() {
+	c1 := make(chan interface{})
+	close(c1)
+	c2 := make(chan interface{})
+	close(c2)
 
-	fmt.Println("Blocking on read...")
+	var c1Count, c2Count int
+	for i := 1000; i >= 0; i-- {
+		select {
+		case <-c1:
+			c1Count++
+		case <-c2:
+			c2Count++
+		}
+	}
+
+	fmt.Printf("c1Count: %d\nc2Count: %d\n", c1Count, c2Count)
+}
+
+func main() {
+	var c <-chan int
 	select {
 	case <-c:
-		fmt.Printf("Unblocked %v later.\n", time.Since(start))
+	case <-time.After(1 * time.Second):
+		fmt.Println("Timed out.")
 	}
 }
