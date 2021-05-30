ce682bc61d92e24f2f04614bc9860ff7f18411c0
n4to4 <n4to4k@gmail.com>
2021-05-31 08:44:42 +0900

    snapshot

--- 04/error-handling.go
+++ 04/error-handling.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func main() { example01() }
+func main() { example02() }
 
 func example01() {
 	checkStatus := func(
@@ -37,5 +37,42 @@ func example01() {
 	urls := []string{"https://www.google.com", "https://badhost"}
 	for response := range checkStatus(done, urls...) {
 		fmt.Printf("Response: %v\n", response.Status)
+	}
+}
+
+func example02() {
+	type Result struct {
+		Error    error
+		Response *http.Response
+	}
+	checkStatus := func(done <-chan interface{}, urls ...string) <-chan Result {
+		results := make(chan Result)
+		go func() {
+			defer close(results)
+
+			for _, url := range urls {
+				var result Result
+				resp, err := http.Get(url)
+				result = Result{Error: err, Response: resp}
+				select {
+				case <-done:
+					return
+				case results <- result:
+				}
+			}
+		}()
+		return results
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	urls := []string{"https://www.google.com", "https://badhost"}
+	for result := range checkStatus(done, urls...) {
+		if result.Error != nil {
+			fmt.Printf("error: %v", result.Error)
+			continue
+		}
+		fmt.Printf("Responses: %v\n", result.Response.Status)
 	}
 }
