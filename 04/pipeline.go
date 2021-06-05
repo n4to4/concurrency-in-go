453fd35a4360487ab3ac29898e959f0ca4cc9493
n4to4 <n4to4k@gmail.com>
2021-06-05 14:09:20 +0900

    snapshot

--- 04/pipeline.go
+++ 04/pipeline.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	pipeline1()
+	pipeline2()
 }
 
 func pipeline0() {
@@ -91,5 +91,53 @@ func pipeline1() {
 
 	for v := range pipeline {
 		fmt.Println(v)
+	}
+}
+
+func pipeline2() {
+	repeat := func(
+		done <-chan interface{},
+		values ...interface{},
+	) <-chan interface{} {
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for {
+				for _, v := range values {
+					select {
+					case <-done:
+						return
+					case valueStream <- v:
+					}
+				}
+			}
+		}()
+		return valueStream
+	}
+
+	take := func(
+		done <-chan interface{},
+		valueStream <-chan interface{},
+		num int,
+	) <-chan interface{} {
+		takeStream := make(chan interface{})
+		go func() {
+			defer close(takeStream)
+			for i := 0; i < num; i++ {
+				select {
+				case <-done:
+					return
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	for num := range take(done, repeat(done, 1), 10) {
+		fmt.Printf("%v ", num)
 	}
 }
